db3c154ebebb20613125b1669483450725c79a63
Laser Yuan <laser.yuan@gmail.com>
2019-09-26 09:25:32 +0800

    Build with docker buildx

--- retropie/install.sh
+++ retropie/install.sh
@@ -22,4 +22,4 @@ if [[ ! -s "$JS_SCRIPTMODULE_FULL" ]]; then
     exit 1
 fi
 
-sudo "$RP_SETUP_DIR/retropie_packages.sh" "$JS_SCRIPTMODULE"
+sudo __platform=$__platform "$RP_SETUP_DIR/retropie_packages.sh" "$JS_SCRIPTMODULE"
