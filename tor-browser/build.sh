2ef00706306388961f695fe8b13eba605a9b9869
Laser Yuan <laser.yuan@gmail.com>
2019-11-29 22:01:05 +0800

    tor-browser: fix permission issue

--- tor-browser/build.sh
+++ tor-browser/build.sh
@@ -2,11 +2,11 @@
 main() {
   case "$1" in
     push)
-docker tag ${REPO}:amd64 lasery/${REPO}:amd64-${VERSION}
-docker push lasery/${REPO}:amd64-${VERSION}
-
 docker tag ${REPO}:amd64 lasery/${REPO}
 docker push lasery/${REPO}
+
+docker tag ${REPO}:amd64 lasery/${REPO}:amd64-${VERSION}
+docker push lasery/${REPO}:amd64-${VERSION}
       ;;
     *)
       exec "$@"
