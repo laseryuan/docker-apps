41522327e4c68b2a57f3f040730661fab21cf278
Laser Yuan <laser.yuan@gmail.com>
2022-12-07 06:03:41 +0000

    feat: add bash

---
+++ bash/debugger.sh
@@ -0,0 +1,10 @@
+debugger() {
+  echo "Stopped in REPL. Press ^D to resume, or ^C to abort."
+  local line
+  while read -r -p "> " line; do
+    eval "$line"
+  done
+  echo
+}
+
+export -f debugger
