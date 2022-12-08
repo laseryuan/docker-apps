4a1a75a73bb329838e8e701ba0292bc2162fc43b
Laser Yuan <laser.yuan@gmail.com>
2022-12-08 07:01:28 +0000

    fix (debugger): not print message to screen when called from $()

--- bash/debugger.sh
+++ bash/debugger.sh
@@ -1,5 +1,5 @@
 debugger() {
-  echo "Stopped in REPL. Press ^D to resume, or ^C to abort."
+  echo "Stopped in REPL. Press ^D to resume, or ^C to abort." >&2
   local line
   while read -r -p "> " line; do
     eval "$line"
