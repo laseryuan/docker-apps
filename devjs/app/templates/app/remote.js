bb3653c0d92cdce24d0ce5d049ccb842f4e9573f
Laser Yuan <laser.yuan@gmail.com>
2023-10-29 10:54:04 -0700

    feat: the workflow that can work with headless browser

--- devjs/app/templates/app/remote.js
+++ devjs/app/templates/app/remote.js
@@ -1,19 +1,22 @@
-// https://medium.com/@trivedidarshan30/remote-debugging-in-puppeteer-to-debug-browser-automation-709b6a578d
 const puppeteer = require('puppeteer');
 
 async function start() {
     const browser = await puppeteer.launch({
      executablePath: "google-chrome",
-     headless: true,
+     headless: "new",
      args: ['--remote-debugging-port=9222', '--remote-debugging-address=0.0.0.0', '--no-sandbox'],
     });
 
     const page = await browser.newPage();
 
-    eval(require('locus'));
+    // Configure the navigation timeout
+    await page.setDefaultNavigationTimeout(0);
 
+    // debugger;
+    eval(require('locus'));
     // start crconsole
-    await page.goto("http://127.0.0.1:8080/breakpoint.html");
+
+    // await page.goto( "http://app:3000/breakpoint.html" );
     // await page.reload();
 
     const version = await browser.version();
